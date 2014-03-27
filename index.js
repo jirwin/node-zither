fe7209b1b13c6d723e75a63c9d4c54186f099b3d
Justin Gallardo <justin.gallardo@rackspace.com>
2014-03-27 00:18:57 -0700

    Timers and events(counters) working. Can add global plugins, as well as module level plugins.

--- index.js
+++ index.js
@@ -1,32 +1,90 @@
 var async = require('async');
 
 var getEvent = require('./lib/event').getEvent;
+var Timer = require('./lib/timer').Timer;
 
 var handlers = {
   events: [],
   timers: []
 };
 
-
-function Zither(modulename) {
-  this.modulename = modulename;
-  this.events = {};
+var moduleHandlers = {
 };
 
-Zither.prototype.recordEvent = function(label) {
-  var event = getEvent([this.modulename, label].join(':'));
 
-  event.record();
+function Zither(moduleName) {
+  this.moduleName = moduleName;
+  this.events = {};
 
-  async.each(handlers.events, function(eventHandler, callback) {
-    eventHandler(event, callback);
+  moduleHandlers[moduleName] = {
+    events: [],
+    timers: []
+  };
+};
+
+Zither.prototype.dispatchHandlers = function(type, obj) {
+  var errs = [],
+      objHandlers = [];
+
+  function captureHandlerErrors(callback) {
+    return function() {
+      var args = Array.prototype.slice.call(arguments);
+
+      if (args[0]) {
+        errs.push(args[0]);
+        args = args.splice(0, 1, null);
+      }
+
+      callback.apply(null, args);
+    };
+  }
+
+  objHandlers = objHandlers.concat(handlers[type], moduleHandlers[this.moduleName][type]);
+
+  async.each(objHandlers, function(objHandler, callback) {
+    callback = captureHandlerErrors(callback);
+
+    try {
+      objHandler(obj, callback);
+    } catch (err) {
+      callback(err);
+      return;
+    }
   }, function(err) {
-    if (err) {
-      console.log('Error calling event handler', err);
+    if (errs.length) {
+      console.log('Error calling handler', eventErrs[0]);
     }
   });
 
-  console.log(event.metrics);
+};
+
+Zither.prototype.recordEvent = function(label) {
+  var event = getEvent([this.moduleName, label].join(':'));
+
+  event.record();
+
+  this.dispatchHandlers('events', event);
+};
+
+Zither.prototype.work = function(label) {
+  var self = this,
+      workTimer = new Timer(label);
+
+  workTimer.once('stop', function(timer) {
+    self.dispatchHandlers('timers', timer);
+  });
+
+  return workTimer;
+};
+
+Zither.prototype.registerPlugin = function(plugin) {
+  if (plugin.hasOwnProperty('event') && typeof plugin.event === 'function') {
+    moduleHandlers[this.moduleName].events.push(plugin.event);
+  }
+
+  if (plugin.hasOwnProperty('timer') && typeof plugin.timer === 'function') {
+    moduleHandlers[this.moduleName].timers.push(plugin.timer);
+  }
 };
 
 exports.instrument = function(modulename) {
@@ -36,11 +94,11 @@ exports.instrument = function(modulename) {
 };
 
 exports.registerPlugin = function(plugin) {
-  if (plugin.hasOwnProperty('event')) {
+  if (plugin.hasOwnProperty('event') && typeof plugin.event === 'function') {
     handlers.events.push(plugin.event);
   }
 
-  if (plugin.hasOwnProperty('timer')) {
+  if (plugin.hasOwnProperty('timer') && typeof plugin.timer === 'function') {
     handlers.timers.push(plugin.timer);
   }
 }
